b9ca68059e76066c99ae7d8439a4d5ee67879637
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2020-05-15 20:36:23 +0530

    Made COLLECTION_NAME dynamic by querying the db

--- src/fetchData.js
+++ src/fetchData.js
@@ -6,13 +6,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   if (err) throw err;
 
-  const collection = client
+  const configCollection = client
     .db(process.env.DB_NAME)
-    .collection(process.env.COLLECTION_NAME);
+    .collection(process.env.CONFIG_COLLECTION);
 
-  collection.find({}).toArray((err, result) => {
-    if (err) throw err;
-    fs.writeFileSync('./raw_data.json', JSON.stringify(result), 'utf8');
-    client.close();
+  configCollection.find({ currentAssignment: true }).toArray((err, result) => {
+    if (err || result.length == 0 || !result[0].collection) {
+      console.log('unable to get collection name');
+      throw err;
+    }
+    const { collection } = result[0];
+    client
+      .db(process.env.DB_NAME)
+      .collection(collection)
+      .find({})
+      .project({ 'tests.sTrace': 0, 'tests.runnerName': 0 })
+      .toArray((err, result) => {
+        if (err) throw err;
+        console.log('here....');
+        fs.writeFileSync('./raw_data.json', JSON.stringify(result), 'utf8');
+        client.close();
+      });
   });
 });
